14c66423c650deb060a96636627fc8fe9bddc98f
Claudio Cherubino <claudiocherubino@gmail.com>
2014-10-14 17:35:59 -0700

    Disabling fullscreen button when casting

--- CastVideos.js
+++ CastVideos.js
@@ -873,11 +873,13 @@ CastPlayer.prototype.updateMediaControlUI = function() {
   if( this.deviceState == DEVICE_STATE.ACTIVE ) {
     document.getElementById("casticonactive").style.display = 'block';
     document.getElementById("casticonidle").style.display = 'none';
+    this.hideFullscreenButton();
     var playerState = this.castPlayerState;
   }
   else {
     document.getElementById("casticonidle").style.display = 'block';
     document.getElementById("casticonactive").style.display = 'none';
+    this.showFullscreenButton();
     var playerState = this.localPlayerState;
   }
 
@@ -976,7 +978,7 @@ CastPlayer.prototype.showVolumeSlider = function() {
 };    
 
 /**
- * Hide the volume stlider 
+ * Hide the volume slider 
  */
 CastPlayer.prototype.hideVolumeSlider = function() {
   document.getElementById('audio_bg').style.opacity = 0;
@@ -1014,21 +1016,40 @@ CastPlayer.prototype.cancelFullScreen = function() {
 /**
  * Exit fullscreen mode by escape 
  */
-CastPlayer.prototype.changeHandler = function(){                                           
-  if (this.fullscreen) { 
-    document.getElementById('fullscreen_expand').style.display = 'block';
-    document.getElementById('fullscreen_collapse').style.display = 'none';
-    this.fullscreen = false;
+CastPlayer.prototype.changeHandler = function(){
+  this.fullscreen = !this.fullscreen;
+  if (this.deviceState == DEVICE_STATE.ACTIVE) {
+    this.hideFullscreenButton();
   }
   else {
-    document.getElementById('fullscreen_expand').style.display = 'none';
-    document.getElementById('fullscreen_collapse').style.display = 'block';
-    this.fullscreen = true;
+    this.showFullscreenButton();
   }
-};    
+};
 
 /**
- * @param {function} A callback function for the fucntion to start timer 
+ * Show expand/collapse fullscreen button
+ */
+CastPlayer.prototype.showFullscreenButton = function(){
+  if (this.fullscreen) {
+    document.getElementById('fullscreen_expand').style.display = 'none';
+    document.getElementById('fullscreen_collapse').style.display = 'block';
+  }
+  else {
+    document.getElementById('fullscreen_expand').style.display = 'block';
+    document.getElementById('fullscreen_collapse').style.display = 'none';
+  }
+};
+
+/**
+ * Hide expand/collapse fullscreen button
+ */
+CastPlayer.prototype.hideFullscreenButton = function(){
+  document.getElementById('fullscreen_expand').style.display = 'none';
+  document.getElementById('fullscreen_collapse').style.display = 'none';
+};
+
+/**
+ * @param {function} A callback function for the function to start timer 
  */
 CastPlayer.prototype.startProgressTimer = function(callback) {
   if( this.timer ) {
@@ -1107,7 +1128,7 @@ CastPlayer.prototype.addVideoThumbs = function() {
  */
 var mediaJSON = { "categories" : [ { "name" : "Movies",
         "videos" : [ 
-        { "description" : "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
+		    { "description" : "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
               "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" ],
               "subtitle" : "By Blender Foundation",
               "thumb" : "images/BigBuckBunny.jpg",
@@ -1149,37 +1170,37 @@ var mediaJSON = { "categories" : [ { "name" : "Movies",
               "thumb" : "images/ForBiggerMeltdowns.jpg",
               "title" : "For Bigger Meltdowns"
             },
-      { "description" : "Sintel is an independently produced short film, initiated by the Blender Foundation as a means to further improve and validate the free/open source 3D creation suite Blender. With initial funding provided by 1000s of donations via the internet community, it has again proven to be a viable development model for both open 3D technology as for independent animation film.\nThis 15 minute film has been realized in the studio of the Amsterdam Blender Institute, by an international team of artists and developers. In addition to that, several crucial technical and creative targets have been realized online, by developers and artists and teams all over the world.\nwww.sintel.org",
+			{ "description" : "Sintel is an independently produced short film, initiated by the Blender Foundation as a means to further improve and validate the free/open source 3D creation suite Blender. With initial funding provided by 1000s of donations via the internet community, it has again proven to be a viable development model for both open 3D technology as for independent animation film.\nThis 15 minute film has been realized in the studio of the Amsterdam Blender Institute, by an international team of artists and developers. In addition to that, several crucial technical and creative targets have been realized online, by developers and artists and teams all over the world.\nwww.sintel.org",
               "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4" ],
               "subtitle" : "By Blender Foundation",
               "thumb" : "images/Sintel.jpg",
               "title" : "Sintel"
             },
-      { "description" : "Smoking Tire takes the all-new Subaru Outback to the highest point we can find in hopes our customer-appreciation Balloon Launch will get some free T-shirts into the hands of our viewers.",
+			{ "description" : "Smoking Tire takes the all-new Subaru Outback to the highest point we can find in hopes our customer-appreciation Balloon Launch will get some free T-shirts into the hands of our viewers.",
               "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4" ],
               "subtitle" : "By Garage419",
               "thumb" : "images/SubaruOutbackOnStreetAndDirt.jpg",
               "title" : "Subaru Outback On Street And Dirt"
             },
-      { "description" : "Tears of Steel was realized with crowd-funding by users of the open source 3D creation tool Blender. Target was to improve and test a complete open and free pipeline for visual effects in film - and to make a compelling sci-fi film in Amsterdam, the Netherlands.  The film itself, and all raw material used for making it, have been released under the Creatieve Commons 3.0 Attribution license. Visit the tearsofsteel.org website to find out more about this, or to purchase the 4-DVD box with a lot of extras.  (CC) Blender Foundation - http://www.tearsofsteel.org", 
+			{ "description" : "Tears of Steel was realized with crowd-funding by users of the open source 3D creation tool Blender. Target was to improve and test a complete open and free pipeline for visual effects in film - and to make a compelling sci-fi film in Amsterdam, the Netherlands.  The film itself, and all raw material used for making it, have been released under the Creatieve Commons 3.0 Attribution license. Visit the tearsofsteel.org website to find out more about this, or to purchase the 4-DVD box with a lot of extras.  (CC) Blender Foundation - http://www.tearsofsteel.org", 
               "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4" ],
               "subtitle" : "By Blender Foundation",
               "thumb" : "images/TearsOfSteel.jpg",
               "title" : "Tears of Steel"
             },
-      { "description" : "The Smoking Tire heads out to Adams Motorsports Park in Riverside, CA to test the most requested car of 2010, the Volkswagen GTI. Will it beat the Mazdaspeed3's standard-setting lap time? Watch and see...",
+			{ "description" : "The Smoking Tire heads out to Adams Motorsports Park in Riverside, CA to test the most requested car of 2010, the Volkswagen GTI. Will it beat the Mazdaspeed3's standard-setting lap time? Watch and see...",
               "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4" ],
               "subtitle" : "By Garage419",
               "thumb" : "images/VolkswagenGTIReview.jpg",
               "title" : "Volkswagen GTI Review"
             },
-      { "description" : "The Smoking Tire is going on the 2010 Bullrun Live Rally in a 2011 Shelby GT500, and posting a video from the road every single day! The only place to watch them is by subscribing to The Smoking Tire or watching at BlackMagicShine.com",
+			{ "description" : "The Smoking Tire is going on the 2010 Bullrun Live Rally in a 2011 Shelby GT500, and posting a video from the road every single day! The only place to watch them is by subscribing to The Smoking Tire or watching at BlackMagicShine.com",
               "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4" ],
               "subtitle" : "By Garage419",
               "thumb" : "images/WeAreGoingOnBullrun.jpg",
               "title" : "We Are Going On Bullrun"
             },
-      { "description" : "The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.",
+			{ "description" : "The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.",
               "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4" ],
               "subtitle" : "By Garage419",
               "thumb" : "images/WhatCarCanYouGetForAGrand.jpg",
